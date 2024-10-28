b54d13edfe1f857a5df19a129afc1140e9d14b3b
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2024-10-28 22:56:01 +0100

    changed build script and cleaned schema file

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -21,23 +21,3 @@ export const users = pgTable("users", {
     emailIdx: uniqueIndex("idx_users_email").on(table.email),
   };
 });
-
-
-// export const genderEnum = pgEnum("gender", ["male", "female"]);
-
-// export const profiles = pgTable("profiles",{
-//   id: char("id",{length: 7}).primaryKey().$default(()=>nanoid()),
-//   userID: uuid("user_id").notNull().unique().references(()=>users.id,{onDelete: "cascade"}),
-//   firstName: varchar("first_name",{length: 256}).notNull(),
-//   lastName: varchar("last_name",{length: 256}),
-//   nationalID: char("national_id", { length: 8 }).unique().notNull(),
-//   phone: char("phone", { length: 8 }).unique(),
-//   gender: genderEnum("gender").notNull(),
-//   age: smallint("age").notNull(),
-//   createdAt: timestamp("created_at", { withTimezone: true }).notNull().defaultNow(),
-//   updatedAt: timestamp("updated_at", { withTimezone: true }),
-// },(table)=>({
-//   nationalIDIdx: uniqueIndex("idx_profiles_national_id").on(table.nationalID),
-//   userIDIdx: uniqueIndex("idx_profiles_user_id").on(table.userID),
-// }))
-
